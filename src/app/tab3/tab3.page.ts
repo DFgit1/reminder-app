bfa79296f1bf373304aa7071fdeb4c4ea02529e5
DFgit1 <fiorentino.dmf@gmail.com>
2022-12-07 11:20:03 -0700

    added files

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +8,72 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  constructor(private alertController: AlertController) {}
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Reminder Set!',
+      subHeader: 'Get Excited!',
+      message: 'Event saved to Reminders',
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
+
+  async storyAlert() {
+    const alert = await this.alertController.create({
+      header: 'Story Time At Heritage Park',
+      subHeader: 'Date:',
+      message: 'Oct 19 - Dec 14',
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
+
+  async golfAlert() {
+    const alert = await this.alertController.create({
+      header: 'Potion Putt: Mini Golf',
+      subHeader: 'Date:',
+      message: 'Nov 3 - Jan 22',
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
+
+  async jazzAlert() {
+    const alert = await this.alertController.create({
+      header: 'Live Jazz At The Establishment',
+      subHeader: 'Date:',
+      message: 'Nov 3 - Dec 15',
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
+
+  async santaAlert() {
+    const alert = await this.alertController.create({
+      header: 'Santas Enchanted Forest',
+      subHeader: 'Date:',
+      message: 'Nov 12 - Dec 24',
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
+
+  async eventAlert() {
+    const alert = await this.alertController.create({
+      header: 'Book An Event now!',
+      subHeader: 'Date:',
+      message: 'TBD',
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
 
 }
--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -4,19 +4,19 @@ import { TabsPage } from './tabs.page';
 
 const routes: Routes = [
   {
-    path: 'tabs',
+    path: 'reminder',
     component: TabsPage,
     children: [
       {
-        path: 'tab1',
+        path: 'home',
         loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
       },
       {
-        path: 'tab2',
+        path: 'events',
         loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
       },
       {
-        path: 'tab3',
+        path: 'reminders',
         loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
       },
       {
